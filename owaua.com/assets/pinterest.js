77808c949f4ee937196031962e65849c2c34aed5
ckazros <ckazro@ckazross-MacBook-Air.local>
2026-09-19 17:13:49 +0200

    Add Pinterest conversion tag

---
+++ owaua.com/assets/pinterest.js
@@ -0,0 +1,7 @@
+/* Pinterest base tag. Keep this loaded once per page, before any event tags. */
+!function(e){if(!window.pintrk){window.pintrk=function(){window.pintrk.queue.push(Array.prototype.slice.call(arguments))};var
+n=window.pintrk;n.queue=[];n.version="3.0";var
+t=document.createElement("script");t.async=!0;t.src=e;var
+r=document.getElementsByTagName("script")[0];r.parentNode.insertBefore(t,r)}}("https://s.pinimg.com/ct/core.js");
+pintrk("load", "2612904762430");
+pintrk("page");
